4bb01be0666a10985b02a8d4c0587fe4f858b0a1
b0lt <3079085+b0lt@users.noreply.github.com>
2018-12-28 15:22:10 -0800

    Move all of the configurable variables into one struct.
    Prelude to serialization/deserialization from URL fragments.

--- src/main/scala/dx2plan/Dx2Plan.scala
+++ src/main/scala/dx2plan/Dx2Plan.scala
@@ -14,10 +14,11 @@ import rx._
 // Identifier of demon based on input box layout (i.e. 0 is left-most)
 case class DemonId(id: Int)
 
-case class DemonSelection(demon: Var[Option[Demon]] = Var[Option[Demon]](None),
-                          color: Var[Color] = Var[Color](Color.Clear),
-                          divine: Var[Boolean] = Var[Boolean](false),
-                          lead: Var[Boolean] = Var[Boolean](false))
+case class DemonConfiguration(demon: Var[Option[Demon]] = Var[Option[Demon]](None),
+                              color: Var[Color] = Var[Color](Color.Clear),
+                              divine: Var[Boolean] = Var[Boolean](false),
+                              lead: Var[Boolean] = Var[Boolean](false),
+                              actions: List[Var[Move]] = List.tabulate(Dx2Plan.maxTurnsPerDemon)(_ => Var[Move](Pass)))
 
 @JSExport
 object Dx2Plan {
@@ -28,28 +29,21 @@ object Dx2Plan {
   val maxTurnsPerDemon = 4
   val initialMp = 2
 
-  // Underlying reactive variables for demon selection.
-  val rxSelections: Seq[DemonSelection] = (0 until maxDemonCount).map { _ => DemonSelection() }
-
-  // Actions are stored by demon instead of order, so that if a demon is changed, only that demon's moves are changed.
-  val rxActions = (0 until maxDemonCount).map { id =>
-    DemonId(id) -> ((0 until maxTurnsPerDemon) map { _ => Var[Move](Pass) })
+  // Underlying reactive variables for demon configuration.
+  val rxConfigurations: Map[DemonId, DemonConfiguration] = (0 until maxDemonCount).map { id =>
+    DemonId(id) -> DemonConfiguration()
   }.toMap
 
   // Actually selected demons.
   val rxDemons = Rx {
-    rxSelections.zipWithIndex.filter { case (selection, index) =>
-        !selection.demon().isEmpty
-    }.map { case (selection, index) =>
-      DemonId(index) -> selection
-    }.toMap
+    rxConfigurations.filterNot { case (_, value) => value.demon().isEmpty }
   }
 
   // List of DemonIds ordered by turn order.
   val rxOrdering = Rx {
-    val demonInfo = rxDemons().map { case (id, selection) => {
-      val agi = selection.demon().get.stats(Stat.Agility)
-      val lead = selection.lead()
+    val demonInfo = rxDemons().map { case (id, configuration) => {
+      val agi = configuration.demon().get.stats(Stat.Agility)
+      val lead = configuration.lead()
       (id, agi, lead)
     }}
     val agiSorted = demonInfo.toSeq.sortWith((left, right) => left._2 > right._2)
@@ -66,7 +60,7 @@ object Dx2Plan {
         case None => {
           val initialState = Rx {
             val demons = rxDemons()
-            val mp = demons.map { case (index, selection) => (index, initialMp) }.toMap
+            val mp = demons.map { case (index, configuration) => (index, initialMp) }.toMap
             val ordering = rxOrdering()
             val firstDemonId = ordering(0)
             val divine = demons(firstDemonId).divine()
@@ -91,7 +85,7 @@ object Dx2Plan {
 
             val previousDemonId = ordering((n - 1) % demons.size)
             val previousDemonTurn = (n - 1) / demons.size
-            val rxPreviousDemonMove = rxActions(previousDemonId)(previousDemonTurn)
+            val rxPreviousDemonMove = demons(previousDemonId).actions(previousDemonTurn)
             prev.evaluate(previousDemonId, rxPreviousDemonMove()).regenMp(demonId, divine)
           }
           lb += nextState
@@ -103,14 +97,15 @@ object Dx2Plan {
   }
 
   val rxDemonSkills = ((0 until maxDemonCount) map { i: Int => {
-    (DemonId(i) -> Rx {
-      val selection = rxSelections(i)
+    val demonId = DemonId(i)
+    (demonId -> Rx {
+      val configuration = rxConfigurations(demonId)
       val skills = ListBuffer[Move]()
-      selection.demon() foreach { demon =>
+      configuration.demon() foreach { demon =>
         demon.baseSkills.foreach { skill =>
           skills += Spell(skill.name, skill.cost.getOrElse(0))
         }
-        demon.awakenSkills(selection.color()) match {
+        demon.awakenSkills(configuration.color()) match {
           case Some(skill) => {
             skills += Spell(skill.name, skill.cost.getOrElse(1) - 1)
           }
@@ -134,7 +129,8 @@ object Dx2Plan {
     }
   }
 
-  def generateDemonSelection(idx: Int) = {
+  def generateDemonConfiguration(demonId: DemonId) = {
+    val idx = demonId.id
     val demonNameId = s"demon${idx}";
     val demonArchetypeId = s"demon${idx}Archetype";
     val demonDivineId = s"demon${idx}Divine";
@@ -146,14 +142,14 @@ object Dx2Plan {
         tabindex := idx * 10 + 1,
         autocomplete := "false",
         oninput := ({(elem: HTMLInputElement) => {
-          rxSelections(idx).demon() = Demon.find(elem.value)
+          rxConfigurations(demonId).demon() = Demon.find(elem.value)
         }}: js.ThisFunction)
       ),
       select(
         id := demonArchetypeId,
         tabindex := idx * 10 + 2,
         oninput := ({(elem: HTMLSelectElement) => {
-          rxSelections(idx).color() = elem.value match {
+          rxConfigurations(demonId).color() = elem.value match {
             case "clear" => Color.Clear
             case "red" => Color.Red
             case "yellow" => Color.Yellow
@@ -172,7 +168,7 @@ object Dx2Plan {
         tabindex := idx * 10 + 3,
         `type` := "checkbox",
         onchange := ({(elem: HTMLInputElement) => {
-          rxSelections(idx).divine() = elem.checked
+          rxConfigurations(demonId).divine() = elem.checked
         }}: js.ThisFunction)
       ),
       input(
@@ -180,14 +176,14 @@ object Dx2Plan {
         tabindex := idx * 10 + 4,
         `type` := "checkbox",
         onchange := ({(elem: HTMLInputElement) => {
-          rxSelections(idx).lead() = elem.checked
+          rxConfigurations(demonId).lead() = elem.checked
         }}: js.ThisFunction)
       ),
-      skillDescriptions(DemonId(idx))
+      skillDescriptions(demonId)
     )
   }
 
-  val moveSelectionTable = Rx {
+  val moveConfigurationTable = Rx {
     val demons = rxDemons()
     val ordering = rxOrdering()
     require(demons.size == ordering.size)
@@ -197,14 +193,16 @@ object Dx2Plan {
       val demonOrder = turn % ordering.size
       val demonId = ordering(demonOrder)
 
-      val demon = demons(demonId).demon()
-      require(!demon.isEmpty)
+      val demon = demons(demonId)
+      require(!demon.demon().isEmpty)
+
+      val demonName = demon.demon().get.name
 
       val rxGameState = rxGameStates(turn)
       val mp = rxGameState().demonMp(demonId)
       val pressTurns = rxGameState().pressTurns
       if (pressTurns > 0) {
-        val rxSelectedAction = rxActions(demonId)(round)
+        val rxSelectedAction = demon.actions(round)
         val rxSkills = rxDemonSkills(demonId)
         val skills = rxSkills() filter { skill => skill.mpCost > 0 }
         val moves: Seq[Move] = Seq(Pass, Attack) ++ skills
@@ -223,7 +221,8 @@ object Dx2Plan {
                   if (selectedAction == move) checked := true,
                   onchange := ({(elem: HTMLInputElement) => {
                     val move = Move.fromString(elem.value)
-                    rxActions(demonId)(round)() = move
+                    val demon = rxConfigurations(demonId)
+                    demon.actions(round)() = move
                   }}: js.ThisFunction)
                 ),
                 label(
@@ -249,7 +248,7 @@ object Dx2Plan {
         Some(
           tr(
             th(
-              p(demon.get.name),
+              p(demonName),
               p(s"$mp MP"),
               p(s"$pressTurns press turns"),
             ),
@@ -268,36 +267,36 @@ object Dx2Plan {
   def main(): Unit = {
     dom.document.body.innerHTML = ""
 
-    val demonSelectionElements = (0 until maxDemonCount) map generateDemonSelection
+    val demonConfigurationElements = (0 until maxDemonCount) map { i => generateDemonConfiguration(DemonId(i)) }
     dom.document.body.appendChild(
       section(
         table(
           tr(
             th("Demon"),
-            demonSelectionElements.map(elements => td(elements._1))
+            demonConfigurationElements.map(elements => td(elements._1))
           ),
           tr(
             th("Archetype"),
-            demonSelectionElements.map(elements => td(elements._2))
+            demonConfigurationElements.map(elements => td(elements._2))
           ),
           tr(
             th("Divine Brand"),
-            demonSelectionElements.map(elements => td(elements._3))
+            demonConfigurationElements.map(elements => td(elements._3))
           ),
 /*        TODO: Actually implement sorting that takes lead brands into account.
           tr(
             th("Lead Brand"),
-            demonSelectionElements.map(elements => td(elements._4))
+            demonConfigurationElements.map(elements => td(elements._4))
           ),
  */
           tr(
             th("Skills"),
-            demonSelectionElements.map(elements => td(elements._5))
+            demonConfigurationElements.map(elements => td(elements._5))
           ),
         )
       ).render
     )
 
-    dom.document.body.appendChild(section(moveSelectionTable).render)
+    dom.document.body.appendChild(section(moveConfigurationTable).render)
   }
 }
